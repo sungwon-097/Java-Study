08b0b42f925da7198411909ae73d16c17e08da9b
Sungwon <sungwon@Sungwonui-MacBookPro.local>
2021-07-11 20:29:10 +0900

    First Commit

---
+++ 자바의신/one/Chapter8/sungwon/ManageStudent.java
@@ -0,0 +1,22 @@
+package one.Chapter8.sungwon;
+
+public class ManageStudent {
+    public static void main(String[] args){
+        Student student[]=null;
+        ManageStudent manageStudent=new ManageStudent();
+        student=manageStudent.addStudent();
+        manageStudent.printStudent(student);
+    }
+    public Student[] addStudent(){
+        Student[] student = new Student[3];
+        student[0]=new Student("Lim");
+        student[1]=new Student("Min");
+        student[2]=new Student("Sook", "Seoul", "010XXXXXXXX", "ask@godofjava.com");
+        return student;
+    }
+    public void printStudent(Student[] student){
+        for(Student data:student){
+            System.out.println(data);
+        }
+    }
+}

22298ae51b60a5d86490643fc26ac27f94ed44f1
Sungwon <sungwon@Sungwonui-MacBookPro.local>
2021-07-16 23:31:16 +0900

    study

---
+++ 자바의신/one/Chapter10/sungwon/c/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package 자바의신.one.Chapter10.sungwon.c.inheritance;
+
+public class Animal {
+    String name;
+    String kind;
+    int legCount;
+    int iq;
+    boolean hasWing;
+    public void move(){}
+    public void eatFood(){}
+}
---
+++ 자바의신/one/Chapter10/sungwon/c/inheritance/Cat.java
@@ -0,0 +1,5 @@
+package 자바의신.one.Chapter10.sungwon.c.inheritance;
+
+public class Cat extends Animal{
+    
+}

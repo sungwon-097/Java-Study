419a3410f8b056556978e71b300d4cc27967da45
sungwon-097 <csw2757@naver.com>
2021-08-02 18:21:12 +0900

    fixed

---
+++ 자바의신/one/Chapter13/sungwon/c/inner/practice/MyPage.java
@@ -0,0 +1,28 @@
+package c.inner.practice;
+
+public class MyPage {
+    InputBox input;
+    public static void main(String[] args){
+        MyPage mypage= new MyPage();
+        mypage.setUI();
+        mypage.pressKey();
+    }
+    public void setUI(){
+        input=new InputBox();
+        KeyEventListener listener=new KeyEventListener(){
+            @Override
+            public void onKeyDown(){
+                System.out.println("Key Down");
+            }
+            @Override
+            public void onKeyUp(){
+                System.out.println("Key Up");
+            }
+        };
+        input.setKeyListener(listener);
+    }
+    public void pressKey(){
+        input.listenerCalled(InputBox.KEY_DOWN);
+        input.listenerCalled(InputBox.KEY_UP);
+    }
+}

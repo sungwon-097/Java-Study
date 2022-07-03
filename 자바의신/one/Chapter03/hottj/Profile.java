2c4cd2ede98d7198ef2c9c6b4ea04a3b3b179a2f
sungwon-097 <csw2757@naver.com>
2022-07-03 15:57:24 +0900

    fix location

---
+++ 자바의신/one/Chapter03/hottj/Profile.java
@@ -0,0 +1,29 @@
+package one.Chapter03.hottj;
+
+public class Profile {
+    String name;
+    int age;
+
+    public void setName(String str){
+        name = str;
+    }
+
+    public void setAge(int val){
+        age = val;
+    }
+
+    public void printName(){
+        System.out.println(name);
+    }
+
+    public void printAge(){
+        System.out.println(age);
+    }
+    public static void main(String[] args) {
+        Profile profile = new Profile();
+        profile.setName("Min");
+        profile.setAge(20);
+        profile.printName();
+        profile.printAge();
+    }
+}
